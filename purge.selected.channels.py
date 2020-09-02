3b8b7730675ed0c19ac59afb0c5595811387f8f9
kieronia <70661699+kieronia@users.noreply.github.com>
2020-09-02 19:59:21 +0100

    Add files via upload

---
+++ purge.selected.channels.py
@@ -0,0 +1,95 @@
+﻿import discord
+from discord.ext.commands import bot
+from discord.ext import commands
+from discord.ext.commands import bot
+from colorama import init, Fore, Back, Style
+import datetime
+init(convert=True)
+bot = commands.Bot(command_prefix='!')
+
+
+#https://discord.com/developers/applications ,make an app,make a bot,go in OAuth2,select bot,scroll and select admin , then copy the link displayed, paste that into your browser and add it to the server that needs cleaning
+tokentorun = "token"
+channeltodelete = "e"
+roletodelete = "new role"
+#space sensitive
+
+
+
+
+print(f"""
+
+{Fore.CYAN}
+                                                            
+                
+
+ ██████╗██╗     ███████╗ █████╗ ███╗   ██╗███████╗██████╗         
+██╔════╝██║     ██╔════╝██╔══██╗████╗  ██║██╔════╝██╔══██╗
+██║     ██║     █████╗  ███████║██╔██╗ ██║█████╗  ██████╔╝
+██║     ██║     ██╔══╝  ██╔══██║██║╚██╗██║██╔══╝  ██╔══██╗
+╚██████╗███████╗███████╗██║  ██║██║ ╚████║███████╗██║  ██║
+ ╚═════╝╚══════╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝
+                                                                      
+            ██████╗  ██████╗ ████████╗                                
+            ██╔══██╗██╔═══██╗╚══██╔══╝                                
+            ██████╔╝██║   ██║   ██║                                   
+            ██╔══██╗██║   ██║   ██║                                   
+            ██████╔╝╚██████╔╝   ██║                                   
+            ╚═════╝  ╚═════╝    ╚═╝                                   
+                                                                                                                 
+
+!channelclean to clean!
+""")
+
+async def on_ready():
+    await bot.change_presence(activity = discord.Streaming(name="Cleaner bot.", url="https://www.twitch.tv/discord"))
+    print("Ready to clean.")
+
+
+
+@bot.command()
+async def roleclean(ctx):
+    await ctx.message.delete()
+    for role in ctx.message.guild.roles:
+            if role.name == roletodelete:
+                try:
+                    await role.delete()
+                except discord.HTTPException as e:
+                    print(f"Failed to delete role: {role.name}. Likely missing perms")
+                    continue
+                else:
+                    currentDT = datetime.datetime.now()
+                    hour = str(currentDT.hour)
+                    minute = str(currentDT.minute)
+                    second = str(currentDT.second)
+                    print(f"{Fore.RED}[{Fore.WHITE}{hour}:{minute}:{second}{Fore.RED}]{Fore.GREEN} Deleted Role: {role.name}")
+    print(f"\n\n{Fore.CYAN}Cleared all channels called '{roletodelete}'
+
+
+@bot.command()
+async def channelclean(ctx):
+    await ctx.message.delete()
+    for channel in ctx.message.guild.channels:
+        if channel == "Text Channels":
+            continue
+        elif channel == "Voice Channels":
+            continue
+        if channel.name == channeltodelete:
+            try:
+                await channel.delete()
+            except discord.Forbidden as e:
+                print(f"Failed to delete channel {channel.name}. Likely missing perms")
+                continue
+            else:
+                currentDT = datetime.datetime.now()
+                hour = str(currentDT.hour)
+                minute = str(currentDT.minute)
+                second = str(currentDT.second)
+                print(f"{Fore.RED}[{Fore.WHITE}{hour}:{minute}:{second}{Fore.RED}]{Fore.GREEN} Channel Deleted - {channel.name}")
+
+
+
+
+    print(f"\n\n{Fore.CYAN}Cleared all channels called '{channeltodelete}',be careful when adding unknown bots in the future.\nBe especially careful if the bot is in few servers,requires permissions \nor if the person who requested for it to be added seems desperate.\nAny bot with admin could've easily mass banned and wiped your server,\nthat I have no cure for, so do look out in the future!\n\nGood luck and I'm happy I was able to help!")
+
+bot.run(tokentorun)
